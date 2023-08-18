5c8470cdc9510c4d4819447579d519e5196ac170
WhereIMgood <mustapha.benammare.99@gmail.com>
2023-08-18 18:13:35 +0100

    Fixing some issues

--- todolist.py
+++ todolist.py
@@ -3,6 +3,7 @@ import json
 from prettytable import PrettyTable
 from colorama import Fore, Style, init
 import sys
+import shutil
 
 init(autoreset=True)  # Initialize colorama
 
@@ -12,22 +13,30 @@ password = open('user.txt', 'r').read() #change if you want
 with open('todos.json', 'r') as todos:
     todos_data = json.load(todos)
 
-def display_todos(todos_dict):
+def truncate_text(text, max_length):
+    return text[:max_length] + ('...' if len(text) > max_length else '')
+
+
+def display_todos(todos_dict, terminal_width):
     table = PrettyTable()
     table.field_names = ["Todo Name", "Description", "Status"]
     table.border = True  # Add borders
     
+    max_name_width = int(terminal_width * 0.3)  # Adjust these percentages as needed
+    max_desc_width = int(terminal_width * 0.5)
+    max_status_width = int(terminal_width * 0.2)
+    
     for todo_name, todo_info in todos_dict.items():
         name_colored = f"{Fore.WHITE}{todo_name}{Style.RESET_ALL}"
-        desc_colored = f"{Fore.WHITE}{todo_info['todo_description']}{Style.RESET_ALL}"
+        desc_colored = f"{Fore.WHITE}{truncate_text(todo_info['todo_description'], max_desc_width)}{Style.RESET_ALL}"  # Truncate long descriptions
         if todo_info['status'] == "Completed":
             status_colored = f"{Fore.GREEN}{todo_info['status']}{Style.RESET_ALL}"
         elif todo_info['status'] == "Incomplete":
             status_colored = f"{Fore.RED}{todo_info['status']}{Style.RESET_ALL}"
-        table.add_row([name_colored, desc_colored, status_colored])
+        table.add_row([name_colored.ljust(max_name_width), desc_colored.ljust(max_desc_width), status_colored.ljust(max_status_width)])
     
         # Add a separator row made of dashes (-) between rows
-        table.add_row([" " * len(name_colored), " " * len(desc_colored), " " * len(status_colored)])
+        table.add_row(["-" * max_name_width, "-" * max_desc_width, "-" * max_status_width])
     
     # Print the table with borders and separators
     print(table)
@@ -62,6 +71,9 @@ def help():
 
 def dashboard():
     while True:
+        terminal_size = shutil.get_terminal_size()
+        terminal_width = terminal_size.columns
+        terminal_height = terminal_size.lines
         command = input("What do you want to do : ")
 
         if "add todo" in command:
@@ -77,7 +89,7 @@ def dashboard():
             with open('todos.json', 'w') as file:
                 json.dump(todos_data, file, indent=4)
             print(f"Todo '{todo_name}' added successfully!")
-            display_todos(todos_data["Todos"])
+            display_todos(todos_data["Todos"], terminal_width)
         elif "del todo" in command:
             extract_todo_name2 = command.split(" ")
             todo_name2 = extract_todo_name2[-1]
@@ -86,16 +98,16 @@ def dashboard():
                 with open('todos.json', 'w') as file:
                     json.dump(todos_data, file, indent=4)
                 print(f"Todo '{todo_name2}' deleted successfully!")
-                display_todos(todos_data["Todos"])
+                display_todos(todos_data["Todos"], terminal_width)
             else:
                 print(f"Todo '{todo_name2}' not found.")
         elif command == "list todos":
-            display_todos(todos_data["Todos"])
+            display_todos(todos_data["Todos"], terminal_width)
         elif "complete todo" in command:
             extract_todo_name3 = command.split(" ")
             todo_name3 = extract_todo_name3[-1]
             update_todo_status(todo_name3, "Completed")
-            display_todos(todos_data["Todos"])
+            display_todos(todos_data["Todos"], terminal_width)
         elif command == "clear":
         	os.system('clear')
         elif command == "help":
@@ -120,4 +132,4 @@ def auth(password_auth):
     else:
         print("Incorrect Password")
 
-auth(input("Enter your password: "))
+auth("default_password")
